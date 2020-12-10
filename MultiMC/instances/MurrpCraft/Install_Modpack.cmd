c579886d4914802b39b153b0d4dc0a9db409bdeb
Davwyn <dafydddragon@gmail.com>
2020-12-09 19:13:46 -0600

    Trial 2

--- MultiMC/instances/MurrpCraft/Install_Modpack.cmd
+++ MultiMC/instances/MurrpCraft/Install_Modpack.cmd
@@ -10,7 +10,7 @@ goto :modpack_updater
 echo.
 echo Installing MurrpCraft...
 IF NOT EXIST "%~dp0.\svn\svn.exe" goto :error_svn_missing
-\svn\svn.exe checkout https://github.com/Dav-Edward/MurrpCraft.git/trunk . --non-interactive
+.\svn\svn.exe checkout https://github.com/Dav-Edward/MurrpCraft.git/trunk .\.minecraft --non-interactive
 IF %ERRORLEVEL% NEQ 0 goto :error_svn
 echo.
 echo Install complete.
